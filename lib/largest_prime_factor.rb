a28272613ea95bd8a65e047f9e722d73b3906b04
beingy <beingy@gmail.com>
2016-01-26 02:27:53 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+def largest_prime_factor(number)
+  odd_factor =  3
+  largest_odd_factor = 3
+  while Math.sqrt(odd_factor) < number
+    if number % odd_factor == 0
+      if odd_factor > largest_odd_factor
+        largest_odd_factor = odd_factor
+      end
+      number = number / odd_factor
+      odd_factor += 2
+    else
+      odd_factor += 2
+    end
+  end
+  largest_odd_factor
+end
+
+#puts largest_prime_factor(600851475143)
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(big_number)
+    @big_number = big_number
+  end
+
+  def big_number=(calc_number)
+    @big_number = calc_number
+  end
+
+  def number
+    odd_factor =  3
+    largest_odd_factor = 3
+    while Math.sqrt(odd_factor) < @big_number
+      if @big_number % odd_factor == 0
+        if odd_factor > largest_odd_factor
+          largest_odd_factor = odd_factor
+        end
+        @big_number = @big_number / odd_factor
+        odd_factor += 2
+      else
+        odd_factor += 2
+      end
+    end
+    largest_odd_factor
+  end
+end
+
+#puts LargestPrimeFactor.new(13195).number
